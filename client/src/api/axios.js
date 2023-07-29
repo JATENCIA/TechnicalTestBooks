4664354af7a5a2814409b51028d96f28e7d7927b
JATENCIA <atenciajaramilloj@gmail.com>
2023-07-28 21:38:42 -0500

    finished

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://technical-test-books.vercel.app/',
   withCredentials: true
 })
 
